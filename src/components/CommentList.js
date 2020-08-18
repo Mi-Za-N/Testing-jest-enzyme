c0c976b59be9672802cd53054b140e3f08f7c98e
Mi-Za-N <mizanwebdeveloper@gmail.com>
2020-08-18 13:24:50 +0600

    Testing with jest & enzyme

---
+++ src/components/CommentList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const CommentList = (props) => {
+  const renderComments = () => {
+    return props.comments.map((comment) => {
+      return <li key={comment}>{comment}</li>;
+    });
+  };
+
+  return (
+    <div>
+      <h4>Comment List</h4>
+      <ul>{renderComments()}</ul>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { comments: state.comments };
+};
+
+export default connect(mapStateToProps)(CommentList);
